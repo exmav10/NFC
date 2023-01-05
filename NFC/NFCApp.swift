b4604bfa1aadcbd2c5685642d765a64879a15fa3
Aydın Ünal <ayuna@dfds.com>
2023-01-05 10:28:46 +0300

    Initial Commit

---
+++ NFC/NFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCApp.swift
+//  NFC
+//
+//  Created by Aydın ÜNAL on 5.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
